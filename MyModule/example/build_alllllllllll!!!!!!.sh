b5d3c296782d9e7ade9744465636330c138c69dc
LIghtJUNction <LIghtJUNction.me@gmail.com>
2025-06-02 21:16:01 +0800

    release

---
+++ MyModule/example/build_alllllllllll!!!!!!.sh
@@ -0,0 +1,4 @@
+gogogo -s "../gogogo/gogogo.go" -o "./dist" -p all -v 2
+
+echo "构建完成，输出目录：./dist"
+ls -l ./dist
--- MyModule/example/build_gogogo_from_src.sh
+++ MyModule/example/build_gogogo_from_src.sh
@@ -1,2 +1,5 @@
 gogogo -s "../gogogo/gogogo.go" -o "./dist" -p "android/arm64" -v 2 
 
+mv "./dist/gogogo_android_arm64" "../gogogo/bin/gogogo"
+
+rm -r "./dist"
